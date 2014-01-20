80eea9044ce28f902a3f556654d7582dee7c1540
anderschen <chenhf2010@qq.com>
2014-01-20 16:13:56 +0800

    windows phone client

---
+++ WPClient/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using WPClient.Resources;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace WPClient
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // 构造函数
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // 用于本地化 ApplicationBar 的示例代码
+            //BuildLocalizedApplicationBar();
+        }
+
+        private async void btnSync_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("test 1...");
+            string result = "";
+            result = await WaitAsynchronouslyAsync();
+
+            //result = await WaitSynchronously();
+
+            Debug.WriteLine("test 2...");
+            Debug.WriteLine("result={0}", result);
+        }
+
+        public async Task<string> WaitAsynchronouslyAsync()
+        {
+            await Task.Delay(1000 * 5);
+            return "WaitAsynchronouslyAsync finished";
+        }
+
+        public async Task<string> WaitSynchronously()
+        {
+            Thread.Sleep(1000 * 5);
+
+            return "WaitSynchronously finished";
+        }
+
+        private void btnNetwork_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("socket");
+        }
+
+        private void btnReactive_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page/ReactivePage.xaml", UriKind.Relative));
+        }
+
+        // 用于生成本地化 ApplicationBar 的示例代码
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // 将页面的 ApplicationBar 设置为 ApplicationBar 的新实例。
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // 创建新按钮并将文本值设置为 AppResources 中的本地化字符串。
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // 使用 AppResources 中的本地化字符串创建新菜单项。
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
---
+++ WPClient/Page/ReactivePage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Reactive;
+using System.Diagnostics;
+
+namespace WPClient
+{
+    public partial class ReactivePage : PhoneApplicationPage
+    {
+        public ReactivePage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReactive_Click(object sender, RoutedEventArgs e)
+        {
+            IObservable<Int32> input = Observable.Range(1, 15);
+            
+            input.Where(i => i % 2 == 0).Subscribe(x => Debug.WriteLine("{0} ", x));
+        }
+
+        private void btnLinq_Click(object sender, RoutedEventArgs e)
+        {
+            List<Int32> its = Enumerable.Range(1, 15).ToList();
+            its.Where(i => i % 2 == 0).ToList().ForEach(i => Debug.WriteLine("{0} ", i));
+        }
+    }
+}
